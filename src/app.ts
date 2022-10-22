afed6dcdfaf7d36aeef5a09e36ac71231ea1a5e0
Leonardo Henrique <leonardo.hdsrodrigues@gmail.com>
2022-10-22 11:34:56 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import router from '@/app/routes';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+}))
 
 app.use(express.json());
 app.use(router);
